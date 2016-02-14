97ba3db6243bb4921029c8412cee7163cc5819af
Yurii <r1906@ukr.net>
2016-02-14 21:49:08 +0200

    Hide keyboard on large screens

--- js/drova.js
+++ js/drova.js
@@ -139,7 +139,6 @@ $('.choose-len-btn').click(function(){
 //вывод клавиатуры
 $('#diam').focus(function(){
     $('.numbers').show();
-    return false;
 });
 /*//ВВОД С КЛАВИАТУРЫ УСТРОЙСТВА
 $('#diam').keyup(function(){
